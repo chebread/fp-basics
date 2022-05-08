a219e895e25dfa9b34f15fa77e88cb329f83fff8
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-08 14:12:02 +0900

    generator

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const log = console.log;
-// 이터러블/이터레이터 프로토콜
-// 이터러블: `[Symbol.iterator]()` 메서드를 가지고 있는 어떤 값을 `[Symbol.iterator]()` 메서드를 통해 실행했을때 이터레이터를 리턴하도록 되어있는 값
-const list = [1, 2, 3, 4, 5];
-log(list[Symbol.iterator]()); // Array Iterator
+// 제너레이터
+// 제너레이터: 이터레이터를 생성하는 함수
+// 제너레이터를 선언할때는 `function *name` 로 선언한다.
+// 제너레이터에 값을 추가할 때는 `yield(value)` 로 선언한다.
+// 제너레이터의 값에 `next()` 메서드를 실행시, `yield` 로 선언한 첫번째 값이 리턴된다.
+// 또한, `next()` 메서드로 평가를 진행하게 된다면, 이터레이터가 반환된다.
+// 하지만, 이터레이터가 `[Symbol.iteator]()` 를 가지고 있으니, 자기 자신을 다시 반환하는 메서드를 가지고 있는 이터러블이라고 볼 수도 있다.
+// 그리고, Generator에는 마지막에 `return` 이라는 값을 추가할 수 있다.
+// 이 값은, `done` 이 `true` 일때 `return` 에서 정의한 값이 `value` 의 값으로 떨어지게 된다.
+// 제너레이터는 어떠한 상태나 값들을 순회 가능한 값, 이터러블로 만들어 준다고 볼 수 있다.
 
-// 이터레이터: `{ value, done }` 값을 가지고 있는 객체를, `next()` 메서드를 통해서 리턴하는 값
-const iterator = list[Symbol.iterator]();
+function* gen() {
+  yield 1; // yield로 이터레이터 값을 만들 수 있다.
+  yield 2;
+}
+const iterator = gen();
 log(iterator.next()); // { value: 1, done: false }
 log(iterator.next()); // { value: 2, done: false }
-
-/*
-arr[Symbol.iterator]() 메서드를 가지고 있고, 이걸 평가해보면 함수가 리턴되며,
-이 메서드를 실행할때는 이터레이터가 반환되니. 이 이터레이터를 `next()` 메서드로 실행하면,
-Array의 `value` 의 값은 `arr` 의 첫번째 값이 있으며,
-`done` 의 값이 `false` 로 떨어지다가 어느 시점부터 `done` 이 `true` 이고 `value` 가 `undefine` 으로 떨어지게 된다면,
-for...of 문의 순회를 중단하는 형태이다.
-
-그래서 이렇게 `next()` 메서드가 뽑아주는 것과 동일하게. for...of문을 보면,
-`value` 의 들어오는 값을 변수인 `a` 에 담아 출력하고, 그다음 `value` 의 들어오는 값을 출력하고,
-또 출력하다가, `done` 이 `true` 가 되면 for...of 문을 빠져 나오도록 되어 있다.
-*/
+log(iterator.next()); // { value: undefined, done: true }
